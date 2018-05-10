bf6c9f03c81d66a84a68de14a00c15d244f6cc0f
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-05-10 23:02:00 +0200

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        public static void main(String[] args){
+            Calculator calculator = new Calculator();
+
+            int finishedAdd = calculator.add(5, 6);
+            int addTest = calculator.getSum();
+            if (finishedAdd == addTest) {
+                System.out.println("Test add OK");
+            } else {
+                System.out.println("Add error!");
+            }
+            int subtractResult = calculator.subtract(7,3);
+            int subtractTest = calculator.getSubtractResult();
+
+            if (subtractResult == subtractTest) {
+                System.out.println("Test subtract OK");
+            } else {
+                System.out.println("Subtract error!");
+            }
+
+        }
     }
-}
