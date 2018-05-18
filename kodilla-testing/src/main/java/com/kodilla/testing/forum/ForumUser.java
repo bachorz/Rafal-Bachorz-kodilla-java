29a7153bceb8b8091765ebc8cc322d7d18a67470
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-05-18 11:42:53 +0200

    veryfication2

--- kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/forum/ForumUser.java
@@ -70,7 +70,6 @@ public class ForumUser {
         }
         return result;
     }
-    //}
 
     public String getName() {
         return name;
--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -5,20 +5,32 @@ import java.util.ArrayList;
 public class ShapeCollector {
 
     private Shape shape;
-    int numberShape;
 
     private ArrayList<Shape> shapes = new ArrayList<>();
+
+    public ShapeCollector(){
+        this.shape = shape;
+    }
 
     public void addFigure(Shape shape){
         shapes.add(shape);
     }
 
-    public void removeFigure(Shape shape){
-        shapes.remove(shape);
+    public boolean removeFigure(Shape shape){
+        boolean result = false;
+        if (shapes.contains(shape)) {
+            shapes.remove(shape);
+            result = true;
+        }
+            return result;
     }
 
-    public void getFigure(int numberShape){
-        shapes.get(numberShape);
+    public Shape getFigure(int n) {
+        shape = null;
+        if (n >= 0 && n < shapes.size()) {
+            shape = shapes.get(n);
+        }
+        return shape;
     }
 
     public void showFigures(){
