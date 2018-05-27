a9e2f052fa2e6268cfc2db5402676bd1355d591e
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-05-27 14:09:15 +0200

    calculator correction

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,10 +1,27 @@
 package com.kodilla.testing;
 
-//import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+
+        int finishedAdd = calculator.add(5, 6);
+        int addTest = 11;
+        if (addTest == finishedAdd) {
+            System.out.println("Test add OK");
+        } else {
+            System.out.println("Add error!");
+        }
+        int subtractResult = calculator.subtract(7,3);
+        int subtractTest = 4;
+
+        if (subtractResult == subtractTest) {
+            System.out.println("Test subtract OK");
+        } else {
+            System.out.println("Subtract error!");
+        }
 
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing.calculator;
+
+
+
+public class Calculator {
+
+//    private int a;
+//    private int b;
+//    private int sum;
+//    private int subtractResult;
+
+    public int add(int a, int b) {
+        return a+b;
+    }
+
+//
+
+    public int subtract(int a, int b) {
+        return a-b;
+    }
+
+
+}
+
+
