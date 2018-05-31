627d3bd3e0b0c41a0ad31efb1afdba8be8206b92
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-05-31 12:15:03 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,17 +1,20 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
 
     public static void main(String[] args) {
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//
+//        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco + text + deco));
+//        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.toUpperCase() + text.toUpperCase() + deco.toUpperCase()));
+//        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.trim().toUpperCase() + text.trim().toLowerCase() + deco.toUpperCase()));
+//        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.toLowerCase()+ text.toLowerCase()));
 
-        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco + text + deco));
-        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.toUpperCase() + text.toUpperCase() + deco.toUpperCase()));
-        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.trim().toUpperCase() + text.trim().toLowerCase() + deco.toUpperCase()));
-        poemBeautifier.beautify("Abc ", "This is module number seven ", ((deco, text) -> deco.toLowerCase()+ text.toLowerCase()));
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
