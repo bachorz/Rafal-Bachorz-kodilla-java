1ea15ef553bdce99e8f4928f5f4438100ed7261e
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-06-09 20:26:16 +0200

    8.3 to verification

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile (){
+    public void readFile ()throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File (classLoader.getResource("file/names.txt").getFile());
 
@@ -17,13 +17,10 @@ public class FileReader {
                 fileLines.forEach(System.out::println);
 
         } catch (IOException e){
-            System.out.println("Oh no! Something went wrong" + e);
+                throw new FileReaderException();
 
             } finally {
                 System.out.println("I am gonna be here... always");
             }
-
-
-        //System.out.println("Oh no! Something went wrong");
     }
 }
