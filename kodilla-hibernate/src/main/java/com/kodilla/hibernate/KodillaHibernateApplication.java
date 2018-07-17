8f4f7abf5a911dbe0bb0492f42d7629f46f0461d
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-07-17 22:06:16 +0200

    13.1 to verification

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -0,0 +1,12 @@
+package com.kodilla.hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class gradlew buildKodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
+}
