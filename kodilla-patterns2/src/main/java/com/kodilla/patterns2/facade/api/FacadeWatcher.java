cf0610f46533c8c8d67e54d306f5fd47e0d5ce9d
“bachorz_do_github <bachorz.rafal@gmail.com_do_github>
2018-09-07 23:13:00 +0200

    20.3 to verification

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/FacadeWatcher.java
@@ -1,5 +1,7 @@
 package com.kodilla.patterns2.facade.api;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -12,8 +14,24 @@ public class FacadeWatcher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(FacadeWatcher.class);
 
-    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..)) && target(object)")
-    public void logEventFacade (Object object) {
-        LOGGER.info("Registration of the event, was called: " + object.getClass().getName());
+    @Before("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..)) && args(order ,userID) && target(object)")
+    public void logEventFacade (OrderDto order, Object object, Long userID) {
+        LOGGER.info("Registration of the event, was called: " + object.getClass().getName() + ", userId: " + userID + ", order: " + order);
+    }
+
+    @Around("execution(* com.kodilla.patterns2.facade.api.OrderFacade.processOrder(..))")
+    public Object measureTimeFacade (final ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        Object result;
+        try {
+            long start = System.currentTimeMillis();
+            result = proceedingJoinPoint.proceed();
+            long endProcess = System.currentTimeMillis();
+            //Long timeProsess = endProcess-start;
+            LOGGER.info("Process duration: " + (endProcess-start) + "[ms]");
+        } catch (Throwable throwable) {
+            LOGGER.error(throwable.getMessage());
+            throw throwable;
+        }
+        return result;
     }
 }
